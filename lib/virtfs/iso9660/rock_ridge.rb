7daebad29e37bc660acf9a109fd1c20f05978872
Mo Morsi <mmorsi@redhat.com>
2016-06-22 15:53:55 -0400

    Refactor modules, componentize & simplify, uncamelize method names, bring up to standards

--- lib/virtfs/iso9660/rock_ridge.rb
+++ lib/virtfs/iso9660/rock_ridge.rb
@@ -12,160 +12,149 @@ module VirtFS::ISO9660
   class PosixAttributes
     attr_reader :flags
 
-    def initialize(data, suff)
-      @flags = 0
-      @suff = suff
-      @px = RR_PX.decode(data)
+    def initialize(data, suffix)
+      @flags  = 0
+      @suffix = suffix
+      @px     = RR_PX.decode(data)
     end
 
     def mode
-      @px["mode#{@suff}"]
+      @px["mode#{@suffix}"]
     end
 
     def nlinks
-      @px["links#{@suff}"]
+      @px["links#{@suffix}"]
     end
 
     def user
-      @px["user#{@suff}"]
+      @px["user#{@suffix}"]
     end
 
     def group
-      @px["group#{@suff}"]
+      @px["group#{@suffix}"]
     end
 
-    def isFile?
+    def file?
       mode & RR_EXT_SL_FM_FILE != 0
     end
 
-    def isDir?
+    def dir?
       mode & RR_EXT_SL_FM_DIR != 0
     end
 
-    def isSymLink?
+    def symlink?
       mode & RR_EXT_SL_FM_LINK != 0
     end
   end # class PosixAttributes
 
   class SymbolicLink
-    attr_reader :flags
-    attr_accessor :linkData
+    attr_reader   :flags
+    attr_accessor :link_data
 
-    def initialize(data, _suff)
+    def initialize(data, _suffix)
       sl = RR_SL.decode(data)
 
       # Reader data.
-      @flags = sl['flags']
-      @linkData = assembleComponents(sl['components'])
+      @flags     = sl['flags']
+      @link_data = assemble(sl['components'])
     end
 
-    def assembleComponents(data)
-      out = ""; offset = 0
+    def assemble(data)
+      out = ""
+      offset = 0
+
       loop do
-        comp = RR_SL_COMPONENT.decode(data[offset..-1])
+        comp     = RR_SL_COMPONENT.decode(data[offset..-1])
+        comp_len = comp['length']
 
         # Check for referential flags.
-        out += "./" if comp['flags'] & RR_EXT_SLCOMPF_CURRENT != 0
-        out += "../" if comp['flags'] & RR_EXT_SLCOMPF_PARENT != 0
-        out += "/" if comp['flags'] & RR_EXT_SLCOMPF_ROOT != 0
+        out += "./"  if comp['flags'] & RR_EXT_SLCOMPF_CURRENT != 0
+        out += "../" if comp['flags'] & RR_EXT_SLCOMPF_PARENT  != 0
+        out += "/"   if comp['flags'] & RR_EXT_SLCOMPF_ROOT    != 0
 
-        # Advance offset, append content (if any) & check for done.
-        # puts "Component content:"
-        # comp['content'][0, comp['length']].hex_dump(:obj => STDOUT, :meth => :puts, :newline => false)
-        # puts "\n\n"
-        offset += comp['length'] + 2 # compensate for first two bytes of component.
-        next if comp['length'] == 0
-        out = File.join(out, comp['content'][0, comp['length']])
+        # Advance offset
+        offset += comp_len + 2 # compensate for first two bytes of component.
+
+        # check for done.
+        next if comp_len == 0
+
+        # append content
+        out = File.join(out, comp['content'][0, comp_len])
+
         # break if comp['flags'] & RR_EXT_SLCOMPF_CONTINUE == 0
         # Analysis of real data shows the condition is offset >= data len.
         break if offset >= data.length
       end
-      # puts "Total content:"
-      # out.hex_dump(:obj => STDOUT, :meth => :puts, :newline => false)
-      # puts "\n\n"
+
       out
     end
   end # class SymbolicLink
 
   class AlternateName
-    attr_reader :flags
+    attr_reader   :flags
     attr_accessor :name
 
-    def initialize(data, _suff)
-      an = RR_NM.decode(data)
+    def current?
+      flags & RR_EXT_NMF_CURRENT != 0
+    end
 
-      # Check for referential flags.
-      @name = ""
-      @name += "./" if an['flags'] & RR_EXT_NMF_CURRENT != 0
-      @name += "../" if an['flags'] & RR_EXT_NMF_PARENT != 0
-      raise "RR extension NM: RESERVED3 flag is set." if an['flags'] & RR_EXT_NMF_RESERVED3 != 0
+    def parent?
+      flags & RR_EXT_NMF_PARENT  != 0
+    end
+
+    def reserved?
+      flags & RR_EXT_NMF_RESERVED3 != 0
+    end
+
+    def initialize(data, _suffix)
+      an = RR_NM.decode(data)
 
       # Reader data.
       @flags = an['flags']
       @name += an['content']
-    end
-  end # class AlternateName
 
-  # Sparse File.
-  RR_SF_SIGNATURE = "SF"
-  RR_SF = BinaryStruct.new([
-    'L',  'size_hiLE',
-    'L',  'size_hiBE',
-    'L',  'size_loLE',
-    'L',  'size_loBE',
-    'C',  'table_depth',
-  ])
+      # Check for referential flags.
+      @name  = ""
+      @name += "./"  if current?
+      @name += "../" if parent?
 
-  # Um, sparse file is pretty complicated so I'm just going to get the
-  # first three going and then worry about this.
-
-  class SparseFile
-    attr_reader :length, :fileData, :flags
-
-    def initialize(data, suff)
-      @sf = RR_SF.decode(data)
-      @flags = 0
-      @suff = suff
-      raise "Sparse file."
-    end
-
-    def length
-      (@sf["size_hi#{@suff}"] << 32) + @sf["size_lo#{@suff}"]
+      raise "RR extension NM: RESERVED3 flag is set." if reserved?
     end
   end # class AlternateName
 
   class Extension
     attr_reader :length, :ext
 
-    def initialize(data, suff)
+    def initialize(data, suffix)
       # Get extension header, length & data.
       @header = RR_EXT_HEADER.decode(data)
       @length = @header['length']
       data = data[RR_EXT_HEADER_SIZE, @length - RR_EXT_HEADER_SIZE]
-      # puts "Extension data (from Extension):"
-      # data.hex_dump(:obj => STDOUT, :meth => :puts, :newline => false)
-      # puts "\n\n"
 
       # Delegate to extension.
-      @ext = case @header['signature']
-             when RR_PX_SIGNATURE then PosixAttributes.new(data, suff)
-             when RR_PN_SIGNATURE then warnAbout(RR_PN_SIGNATURE, data)
-             when RR_SL_SIGNATURE then SymbolicLink.new(data, suff)
-             when RR_NM_SIGNATURE then AlternateName.new(data, suff)
-             when RR_CL_SIGNATURE then warnAbout(RR_CL_SIGNATURE, data)
-             when RR_PL_SIGNATURE then warnAbout(RR_PL_SIGNATURE, data)
-             when RR_RE_SIGNATURE then warnAbout(RR_RE_SIGNATURE, data)
-             when RR_TF_SIGNATURE then warnAbout(RR_TF_SIGNATURE, data)
-             when RR_SF_SIGNATURE then SparseFile.new(data, suff)
-             end
+      @ext = ext_delegate(data, suffix)
     end
 
-    def warnAbout(ext, data)
-      if $log
-        $log.debug("RR extension #{ext} found, not processed. Data is:")
-        data.hex_dump(:obj => $log, :meth => :debug, :newline => false)
+    def ext_delegate(data, suffix)
+      case @header['signature']
+      when RR_PX_SIGNATURE then PosixAttributes.new(data, suffix)
+      when RR_SL_SIGNATURE then SymbolicLink.new(data, suffix)
+      when RR_NM_SIGNATURE then AlternateName.new(data, suffix)
+      #when RR_SF_SIGNATURE then
+      #when RR_PN_SIGNATURE then
+      #when RR_CL_SIGNATURE then
+      #when RR_PL_SIGNATURE then
+      #when RR_RE_SIGNATURE then
+      #when RR_TF_SIGNATURE then
       end
-      nil
+    end
+
+    def flags
+      ext.flags
+    end
+
+    def continue?
+      obj.flags & CONTINUE
     end
   end # class Extension
 
@@ -173,50 +162,71 @@ module VirtFS::ISO9660
     SUSP_SIZE = 7
     CONTINUE  = 1
 
-    attr_reader :extensions
+    attr_reader :header, :extensions
 
-    def initialize(de, suff)
+
+    def header_offset
+      # Root directories need to skip SUSP header.
+      @header_offset ||= @de.name == "." ? SUSP_SIZE : 0
+    end
+
+    def header
+      @header ||= RR_HEADER.decode(@de.sua[header_offset..-1])
+    end
+
+    def signature
+      header['signature']
+    end
+
+    def valid_signature?
+      signature == RR_SIGNATURE
+    end
+
+    def initialize(de, suffix)
       raise "No DirectoryEntry specified." if de.nil?
       raise "The specified DirectoryEntry has no System Use Area." if de.sua.nil?
-
-      # Root directories need to skip SUSP header.
-      offset = de.name == "." ? SUSP_SIZE : 0
+      @de = de
 
       # Get the RR header from the DirectoryEntry & verify.
-      @header = RR_HEADER.decode(de.sua[offset..-1])
-      raise "This is not a Rock Ridge extension record" if @header['signature'] != RR_SIGNATURE
+      raise "This is not a Rock Ridge extension record" unless valid_signature?
 
-      # Loop through extensions.
-      offset += RR_HEADER_SIZE
+      extract_extensions
+      resolve_continuations
+    end
+
+    private
+
+    def extract_extensions
+      offset = header_offset + RR_HEADER_SIZE
       @extensions = []
       loop do
-        # puts "Extension data:
-        # de.sua[offset..-1].hex_dump(:obj => STDOUT, :meth => :puts, :newline => false)
-        # puts "\n\n"
-        ext = Extension.new(de.sua[offset..-1], suff)
-        @extensions << ext
-        offset += ext.length
+        offset_data  = de.sua[offset..-1]
+        extension    = Extension.new(offset_data, suffix)
+        @extensions << extension
+
+        offset += extension.length
         break if offset >= de.sua.length
       end
+    end
 
-      # Handle continuations.
+    def resolve_continuations
       0.upto(@extensions.size - 1) do |idx|
         obj = @extensions[idx]
         next if obj.nil?
 
-        # TODO: Simplify this - the meat of all extensions is .data
-        if obj.flags & CONTINUE
-          if obj.kind_of?(AlternateName)
+        if obj.continue?
+          if obj.ext.is_a?(AlternateName)
             obj.name += @extensions[idx + 1].name
             @extensions[idx + 1] = nil
-          end
-          if obj.kind_of?(SymbolicLink)
-            obj.linkData += @extensions[idx + 1].linkData
+
+          elsif obj.ext.is_a?(SymbolicLink)
+            obj.link_data += @extensions[idx + 1].link_data
             @extensions[idx + 1] = nil
           end
         end
       end
-      @extensions.delete(nil)
+
+      @extensions.compact!
     end
   end # class RockRidge
 end # module VirtFS::ISO9660
